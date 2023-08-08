054f8cef7d66e0957df10bae4290d2d16be447d8
hironaga-1 <oretachisanta410@gmail.com>
2023-08-09 00:58:42 +0900

    letter_opener_web

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: ENV.fetch("DEFAULT_URL", 'localhost'), port: ENV.fetch("DEFUALT_PORT", 3000) }
+  config.action_mailer.default_url_options = { host: ENV.fetch("DEFAULT_URL", 'localhost'), port: ENV.fetch("DEFAULT_PORT", 3000) }
+
+  config.action_mailer.delivery_method = :letter_opener_web
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
